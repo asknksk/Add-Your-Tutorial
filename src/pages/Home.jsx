002fca8889316a6ea265e9040d98563a934a3816
asknksk <asknkskk@gmail.com>
2022-07-02 12:15:24 +0300

    api added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,29 @@
+import axios from "axios";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState();
+
+  const url = "https://axios-example-cw.herokuapp.com/api/tutorials";
+
+  const getTutorials = async () => {
+    const { data } = await axios.get(url);
+    console.log(data);
+  };
+
+  // Sadece Component mount oldugunda istek yapar
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
+  console.log(tutorials);
+
   return (
     <>
       <AddTutorial />
-      <TutorialList />
+      <TutorialList tutorials={tutorials} />
     </>
   );
 };
